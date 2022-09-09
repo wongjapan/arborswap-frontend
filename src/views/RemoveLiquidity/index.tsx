d3a29cb65f6743fa262eefbb7d98fddf9202349e
WongJapan <kulomilyarder@gmail.com>
2022-09-09 15:01:09 +0700

    fix domain typehash name

--- src/views/RemoveLiquidity/index.tsx
+++ src/views/RemoveLiquidity/index.tsx
@@ -113,7 +113,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'Arbor LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
