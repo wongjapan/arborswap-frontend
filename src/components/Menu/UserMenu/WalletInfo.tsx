e31dfa3b6f305cf8cc15c49f536b650a7452cbd9
WongJapan <kulomilyarder@gmail.com>
2022-11-12 01:50:20 +0700

    change bscscan to explorer

--- src/components/Menu/UserMenu/WalletInfo.tsx
+++ src/components/Menu/UserMenu/WalletInfo.tsx
@@ -41,15 +41,12 @@ const WalletInfo: React.FC<WalletInfoProps> = ({ hasLowBnbBalance, onDismiss }) 
         </Message>
       )}
       <Flex alignItems="center" justifyContent="space-between">
-        <Text color="textSubtle">{t('BNB Balance')}</Text>
+        <Text color="textSubtle">{t('RBA Balance')}</Text>
         <Text>{getFullDisplayBalance(balance, 18, 6)}</Text>
       </Flex>
-      <Flex alignItems="center" justifyContent="space-between" mb="24px">
-        <Text color="textSubtle">{t('CAKE Balance')}</Text>
-        <Text>{getFullDisplayBalance(cakeBalance, 18, 3)}</Text>
-      </Flex>
+
       <Flex alignItems="center" justifyContent="end" mb="24px">
-        <LinkExternal href={getBscScanLink(account, 'address')}>{t('View on BscScan')}</LinkExternal>
+        <LinkExternal href={getBscScanLink(account, 'address')}>{t('View on Explorer')}</LinkExternal>
       </Flex>
       <Button variant="secondary" width="100%" onClick={handleLogout}>
         {t('Disconnect Wallet')}
