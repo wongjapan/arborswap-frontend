f577c5c7b6d02bd7da22cc4bb10413cad422cde5
WongJapan <kulomilyarder@gmail.com>
2022-12-27 09:47:57 +0700

    change liquidity text

--- src/views/AddLiquidity/index.tsx
+++ src/views/AddLiquidity/index.tsx
@@ -311,7 +311,7 @@ export default function AddLiquidity({
           title={t('Add Liquidity')}
           subtitle={t('Add liquidity to receive LP tokens')}
           helper={t(
-            'Liquidity providers earn a 0.17% trading fee on all trades made for that token pair, proportional to their share of the liquidity pool.',
+            'To make ArborSwap the best option for you, we’re providing the lowest service fees on the market: just 0.1% for swaps on the native DEX, and 0.05% for external DEX service requests.',
           )}
           backTo="/pool"
         />
