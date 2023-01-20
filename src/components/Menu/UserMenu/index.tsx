10cf2ecdaa44a6b1c45e9d4cb5c29796d7fd6f11
WongJapan <kulomilyarder@gmail.com>
2023-01-21 00:36:59 +0700

    remove make profile

--- src/components/Menu/UserMenu/index.tsx
+++ src/components/Menu/UserMenu/index.tsx
@@ -33,8 +33,8 @@ const UserMenu = () => {
         {t('Transactions')}
       </UserMenuItem>
       <UserMenuDivider />
-      <ProfileUserMenuItem isLoading={isLoading} hasProfile={hasProfile} />
-      <UserMenuDivider />
+      {/* <ProfileUserMenuItem isLoading={isLoading} hasProfile={hasProfile} /> */}
+      {/* <UserMenuDivider /> */}
       <UserMenuItem as="button" onClick={logout}>
         <Flex alignItems="center" justifyContent="space-between" width="100%">
           {t('Disconnect')}
